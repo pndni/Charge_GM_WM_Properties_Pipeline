7b924465a869b6dbb412fffd671264b602bdf020
Steven Tilley <stilley2@jhmi.edu>
2019-05-07 14:51:00 -0400

    release 1.0.0-alpha11

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -4,7 +4,7 @@ set -e  # exit on error
 set -u  # exit on undefined variable
 \unalias -a  # remove all aliases (e.g. some systems alias 'cp' to 'cp -i')
 
-version=dev
+version=1.0.0-alpha11
 
 error() {
   >&2 echo $1
